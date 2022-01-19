119a0ea63873ce76a38fdac13b354a091ab319f9
Kiryl Yermalovich <ts.underawater@gmail.com>
2022-01-19 17:07:40 +0300

    add: added project

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h2 className="section-title">About us</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Quod magni cupiditate vero dolores illo similique repellendus aliquid,
+        itaque voluptatem ipsam natus beatae iste architecto eveniet sapiente numquam necessitatibus.
+        Harum quaerat excepturi quos facilis in tenetur, exercitationem perspiciatis similique sint? Eos!
+      </p>
+    </section>
+  )
+}
+
+export default About
